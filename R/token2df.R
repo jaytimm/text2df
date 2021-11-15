1bbf634ed2cd29ebe6600a2931ad8b4a287af486
Jason Timm <jatimm@salud.unm.edu>
2021-11-15 13:13:52 -0700

    first

---
+++ R/token2df.R
@@ -0,0 +1,25 @@
+#' Convert tokens to df -- via textshape package
+#'
+#' @name token2df
+#' @param tok A list
+#' @return A data frame
+#'
+#' @export
+#' @rdname token2df
+#'
+#'
+token2df <- function(tok){
+
+  df <- textshape::tidy_list(tok,
+                             id.name = 'doc_id',
+                             content.name = 'token')
+
+  if(grepl('\\.', df$doc_id[1])) {
+    df[, sentence_id := gsub('^.*\\.', '', doc_id)]
+    df[, doc_id := gsub('\\..*$', '', doc_id)]
+  }
+
+  df[, token_id := data.table::rowid(doc_id)]
+  return(df)
+}
+
